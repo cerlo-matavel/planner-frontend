642a04a1eb6bb9ed535d40ff1082864ef54c0a0b
rocks d xebec <cerlomat@gmail.com>
2024-07-16 13:33:35 +0200

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { TripDetailsPage } from "./pages/trip-details";
+import { CreateTripPage } from "./pages/create-trip";
+
+
+//Array com todas as paginas da aplicacao
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateTripPage/>,
+  },
+
+  {path: "/trips/:tripId",
+  element: <TripDetailsPage/>,
+  }
+]);
+
+// React router para o roteamento
+export function App() {
+  return <RouterProvider router={router} />
+}
